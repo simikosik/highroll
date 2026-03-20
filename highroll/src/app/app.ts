0095d9109cb30bfb9008ef6dd09d0716259cc225
AbafiM23 <AbafiM23@Z00T6-01>
2026-03-20 13:16:13 +0100

    BALALAL

--- highroll/src/app/app.ts
+++ highroll/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { routes } from './app.routes';
+import { Balance } from './balance/balance';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Balance],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
