06b2c30dca7618c40b3799aef02491a86bdfc877
MatusAbaf <kiarama787@gmail.com>
2026-04-23 20:37:51 +0200

    test

---
+++ highroll/src/app/popo/popo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popo',
+  imports: [],
+  templateUrl: './popo.html',
+  styleUrl: './popo.css',
+})
+export class Popo {
+
+}
